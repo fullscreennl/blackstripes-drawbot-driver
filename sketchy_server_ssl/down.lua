ea78867fc32e41af73bd16d845ce3f833e4957a5
johantenbroeke <johan@fullscreen.nl>
2018-02-21 15:51:29 +0000

    added penlift calibration mode

--- sketchy_server_ssl/down.lua
+++ sketchy_server_ssl/down.lua
@@ -1,1 +1,26 @@
-penDown();
+penUp()
+
+moveTo(0,1000.0)
+penDown()
+
+penup = false
+local i = 0
+
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
+end
+
+
+while true do
+    sleep(5)
+    if penup == false then
+        moveTo(100.0,1000.0)
+        penUp()
+        penup = true
+    else
+        moveTo(0.0,1000.0)
+        penDown()
+        penup = false
+    end
+end
+
